b17716773925a96fbb19f42e4107ef16fef78724
Benson <bensoniscool123@gmail.com>
2020-06-14 23:44:02 +0800

    no message

--- src/Update.elm
+++ src/Update.elm
@@ -80,11 +80,11 @@ animate time model =
 
         (eliminated_1, rest)= 
             List.partition 
-                (\brick-> ((brick |> (collideWith model.player1.ball))&&brick.pos.y>=0)) 
+                (\brick-> (brick |> collideWith model.player1.ball)) 
                 model.bricks
         (eliminated_2, newrest)=
             List.partition
-                (\brick-> ((brick |> (collideWith model.player2.ball))&&brick.pos.y>=0))
+                (\brick-> (brick |> collideWith model.player2.ball))
                 rest
 
         audioList = 
@@ -120,89 +120,13 @@ animate time model =
             , bricks=bricks
             , audioList=audioList
         }
-formPongs bricks = 
-    let
-        pongs = bricks
-            |> List.take 3
-        brick1 = pongs
-            |> List.take 1
-        brick2 = pongs
-            |> List.drop 1
-            |> List.take 1
-
-        brick3 = pongs
-            |> List.drop 2
-            |> List.head 
-    in
-        case brick3 of
-            Nothing -> 
-                False
-            Just brick ->
-                (List.member brick brick1) && (List.member brick brick2)
-
-vaildKong suit =
-    ( suit <=27 ) && ( modBy 9 suit /=0 ) && ( modBy 9 suit /=8 )
-
-formKongHelper brick bricks =
-    if vaildKong brick.suit then
-        (List.member (brick.suit+1) (List.map .suit bricks))
-        && (List.member (brick.suit+2) (List.map .suit bricks))
-    else 
-        False
-
-formKong bricks = 
-    case (List.head bricks) of
-        Nothing -> 
-            False
-        Just brick ->
-            formKongHelper brick bricks
-
-formChow bricks = 
-    let
-        chow = bricks
-            |> List.take 2 
-        brick1 = chow
-            |> List.take 1
-        brick2 = chow
-            |> List.drop 1
-            |> List.head 
-        
-    in
-        case brick2 of
-            Nothing -> 
-                False
-            Just brick ->
-                List.member brick brick1
-
-dropBrick suit bricks =
-    let
-        ( sameBricks,rest ) = List.partition (\brick -> brick.suit == suit) bricks
-    in
-        List.append rest (List.drop 1 sameBricks)
+--formPongs bricks = 
 
 
-dropKong bricks =
-    let
-        suit1 = case (List.head bricks) of
-            Nothing -> 
-                0
-            Just brick ->
-                brick.suit
-    in
-        bricks
-        |> dropBrick suit1
-        |> dropBrick (suit1 + 1)
-        |> dropBrick (suit1 + 2)
+--formKong bricks = 
 
+--formChow bricks = 
 
-formHu bricks =
-    if List.length bricks == 0 then
-        True
-    else
-        ( (formPongs bricks) && (formHu (List.drop 3 bricks)) )
-        || ( (formKong bricks) && 
-            (formHu (dropKong bricks)) )
-        || ( (modBy 3 (List.length bricks) == 2)&&(formChow bricks)&&(formHu (List.drop 2 bricks)) )
 
 
 moveBricks bricks =
@@ -229,10 +153,7 @@ collideWithPaddle paddle brick =
   &&((paddle.pos.x+paddle.speed)< (brick.pos.x+brick.size.x)))
 
 handcardSetOff handcard=
-    (Model.attribute.range.x-(toFloat(List.length handcard))*Model.brickWidth*handcardSizeRate)/2
-
-handcardSizeRate =
-    (toFloat Model.attribute.bricksNum.x) /(toFloat Model.attribute.handcardNum)
+    (Model.attribute.range.x-(toFloat(List.length handcard))*Model.brickWidth)/2
 
 catchHandcard player =
     let
@@ -247,7 +168,7 @@ catchHandcard player =
             |>List.map2 
                 (\posx card->
                     {card | 
-                        pos=Vector (posx*handcardSizeRate+ handcardSetOff(List.append handcard player.handcard)) Model.attribute.handcardPosY}
+                        pos=Vector (posx+ handcardSetOff(List.append handcard player.handcard)) Model.attribute.handcardPosY}
                     ) (Model.posXList 13)
         newFallingcard=List.filter (\card->card.pos.y<(Model.attribute.range.y*2/3+25)) fallingcard
     in
@@ -350,8 +271,6 @@ punish ball=
         {ball | punish=False}
     else
         {ball|speed= addVector ball.speed (Vector 0 0.01) }
-
-    
 
 
 changeBallSpeed paddle ball=
